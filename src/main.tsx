a7d0f049baf3154463e0c41aaaef11c395a05050
Ashma Upreti <ashma.upreti99@gmail.com>
2023-07-25 23:04:25 -0400

    create routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/home-page/home-page.tsx";
+import ProductDetailPage from "./pages/product-detail-page/product-detail.tsx";
+import ProductListPage from "./pages/product-list-page/product-list-page.tsx";
+import Register from "./pages/register/register.tsx";
+import SignIn from "./pages/sign-in/sigin-in.tsx";
+import ErrorPage from "./pages/error-page/error-page.tsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/each-item",
+    element: <ProductDetailPage />,
+  },
+  {
+    path: "/Products",
+    element: <ProductListPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/error",
+    element: <ErrorPage />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
